968ffcb8177c95fef94379efc8fde5203a4912a1
deb12356 <debiprasad.biswal90@gmail.com>
2019-08-08 00:08:09 +0530

    added space

--- EF6DBFirstDemo/Program.cs
+++ EF6DBFirstDemo/Program.cs
@@ -11,6 +11,7 @@ namespace EF6DBFirstDemo
     {
         private static void Main(string[] args)
         {
+
             AddUpdateDeleteEntityInConnectedScenario();
             AddUpdateEntityInDisconnectedScenario();
             LinqToEntitiesQueries();
